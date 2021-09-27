781d6bb59b23f9c76a1066640d9c8dd3024fcaeb
Rcaradec <rcaradec.dev@gmail.com>
2021-09-27 22:58:21 +0200

    grille prête

--- js/app.js
+++ js/app.js
@@ -1,16 +1,33 @@
 var app = {
   init: function () {
     console.log("init !");
+    app.drawBoard();
   },
   player: {
     x: 0,
     y: 0,
     direction: "right",
   },
-
   targetCell: () => {
     x = 5;
     y = 3;
+  },
+  drawBoard: () => {
+    const board = document.getElementById("board");
+    // je veux créer 4 lignes en div
+    for (let i = 0; i < 4; i++) {
+      let row = document.createElement("div");
+      row.className = "row";
+      console.log(row);
+      // Je veux rajouter 6 Div dans mes 4 row
+      for (let j = 0; j < 6; j++) {
+        let cell = document.createElement("div");
+        cell.className = "cell";
+        console.log(cell);
+        row.appendChild(cell);
+      }
+      board.appendChild(row);
+    }
   },
 };
 
